8d683aa9b64bc54df15ca7110b52affc0b6afae2
cbv <cbv@Carls-MacBook-Pro.local>
2013-11-16 00:37:34 -0700

    Should reduce memory overhead.

--- 3/e3.py
+++ 3/e3.py
@@ -4,7 +4,9 @@ from factorization.factors import factors
 
 def main():
 	n = 600851475143
-	largest = [p for p in factors(n)][-1]
+	largest = 0
+	for p in factors(n):
+		largest = p
 	print('largest = {0}'.format(largest))
 
 if __name__ == '__main__':
