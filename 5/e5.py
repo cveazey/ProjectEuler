2a4297f95456465737b4b78eef5f7f3cda5af0f9
cbv <cbv@Carls-MacBook-Pro.local>
2013-11-20 00:01:26 -0700

    Use gcd and lcm instead of prime factorization for problem 5.

--- 5/e5.py
+++ 5/e5.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python
 
-from factorization.factors import factors
 
-def smallest_multiple(n):
-	"""Returns smallest multiple evenly divisble by each 1..n in N
+def gcd(a,b):
+	"""Returns the greatest common divisor of a and b
 
-	>>> smallest_multiple(10)
-	2520
+	>>> gcd(100,90)
+	10
+	>>> gcd(1000,750)
+	250
 
 	"""
+	while b:
+		a, b = b, a % b
+	return a
 
-	# for each number <= n, find its prime factors p1^k1, p2^k2, ...
-	# 	store as prime => exponent
-	# 		replace existing stored exponents only if the replacement is greater
-	# for each prime=>exponent
-	# 	result = result * prime^exponent
-	factor_exp_map = {}
-	for i in xrange(n,0,-1):
-		facs = list(factors(i))
-		for factor in facs:
-			fact_count = facs.count(factor)
-			cur_fact_count = factor_exp_map.get(factor,0)
-			if (fact_count > cur_fact_count):
-				factor_exp_map[factor] = fact_count
-	result = 1
-	for factor, exponent in factor_exp_map.iteritems():
-		result = result * factor**exponent
-	return result
+def lcm(a,b):
+	"""Returns least common multiple of a and b
+	a and b being pos integers
+	>>> lcm(12,58)
+	348
+	>>> lcm(10,14)
+	70
+	"""
+	numerator = a*b
+	denominator = gcd(a,b)
+	return numerator / denominator
+	
+def lcm_series(n):
+	"""Returns lcm of each 1..n
+
+	>>> lcm_series(10)
+	2520
+	>>> lcm_series(20)
+	232792560
+
+	"""
+	if n == 1:
+		return n
+	return lcm(n,lcm_series(n-1))
 
 def main():
-	print('result = {0}'.format(smallest_multiple(20)))
+	print('result = {0}'.format(lcm_series(20)))
 	
 if __name__ == '__main__':
 	main()
